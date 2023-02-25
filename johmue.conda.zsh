058e9d728092e92ad5e27d0f23a3a142144f5ac2
Johannes Mueller <github@johannes-mueller.org>
2023-02-25 17:11:19 +0100

    Not activate conda envs after every command

--- johmue.conda.zsh
+++ johmue.conda.zsh
@@ -1,15 +1,23 @@
 
 conda config --set changeps1 false
 
-precmd_conda_info() {
+activate_local_conda_env() {
     if [[ -d ./.venv/conda-meta ]] && [[ $CONDA_PREFIX != "$(pwd)/.venv" ]] ; then
 	conda activate ./.venv
     fi
+}
+
+activate_local_conda_env
+
+autoload -U add-zsh-hook
+add-zsh-hook chpwd activate_local_conda_env
+
+precmd_conda_info() {
     if [[ -n $CONDA_PREFIX ]]; then
        if [[ $(basename $CONDA_PREFIX) == "miniconda3" ]]; then
           CONDA_ENV="(base) "
        else
-         CONDA_ENV="($(echo $CONDA_PREFIX | sed s%$PWD%.% | sed s%$HOME%~%)) "
+         CONDA_ENV="($(echo $CONDA_PREFIX | sed s%$HOME/miniconda3/envs/%%\;s%$PWD%.%\;s%$HOME%~% )) "
        fi
     else
       CONDA_ENV=""
