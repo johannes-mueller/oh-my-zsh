80c951dcb7b1c3b788e5b05969834042df3000a4
Johannes Mueller <github@johannes-mueller.org>
2023-11-27 09:34:41 +0100

    Make conda setup honor /home/jmu3si

--- johmue.conda.zsh
+++ johmue.conda.zsh
@@ -1,12 +1,12 @@
 
-__conda_setup="$('/home/joh/.miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup=`($HOME/.miniconda3/bin/conda 'shell.zsh' 'hook' 2> /dev/null)`
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/joh/.miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/joh/.miniconda3/etc/profile.d/conda.sh"
+    if [ -f "${HOME}/.miniconda3/etc/profile.d/conda.sh" ]; then
+        . "${HOME}/.miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/home/joh/.miniconda3/bin:$PATH"
+        export PATH="${HOME}/.miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
