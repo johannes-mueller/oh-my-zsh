563110bceb4cf0b8994cf3b9dcff0f11d77de0d1
Johannes Mueller <github@johannes-mueller.org>
2025-01-22 09:56:32 +0100

    Switch from conda to uv

---
+++ johmue.uv.zsh
@@ -0,0 +1,13 @@
+
+activate_local_uv_env() {
+    if [[ -e ./.venv/pyvenv.cfg ]] && [[ $UV_PREFIX != "$(pwd)/.venv" ]] ; then
+	source ./.venv/bin/activate
+    fi
+}
+
+source $HOME/.miniconda3/bin/activate
+
+activate_local_uv_env
+
+autoload -U add-zsh-hook
+add-zsh-hook chpwd activate_local_uv_env
